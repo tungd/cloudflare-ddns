01eb8b5d80034454de0068f4bfdb8bd0f19af172
Tung Dao <me@tungdao.com>
2018-07-08 11:28:17 +0700

    basic layout

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,21 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
+
+import RIO
+
+
+data Config = Config
+  { email :: Text
+  , apiKey :: Text
+  , domain :: Text
+  } deriving (Eq, Show, Generic)
+
+
+config = Config
+  { email = "me@tungdao.com"
+  , apiKey = "ed3320f7ce5e08facb4b138778ad4b62960cb"
+  , domain = "mbp.tungdao.com"
+  }
+
 
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = undefined
